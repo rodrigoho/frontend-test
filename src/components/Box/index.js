1f6516a38c3b525e9510428ec9cf40e5533d740e
Rodrigo Hoshino Okiyama <hoshino.rodrigo@yahoo.com.br>
2020-06-09 02:09:50 -0300

    Implement Box component to use globally

---
+++ src/components/Box/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Box = styled.div`
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  margin: 80px auto;
+`;
+
+export default Box;
